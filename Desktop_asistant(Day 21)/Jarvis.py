5fdcbf4ce7df2c2ff04f962a4a5219d7d5950938
Sattwik Palai <sattwikpalai21@gmail.com>
2021-07-10 10:33:33 +0530

    further changes

--- Desktop_asistant(Day 21)/Jarvis.py
+++ Desktop_asistant(Day 21)/Jarvis.py
@@ -94,16 +94,18 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "C:\\Users\\Sattwik Palai\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
         elif 'send audio' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
+                to = "sattwikpalai21@gmail.com"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend  sattwik bhai. I am not able to send this email")    
+                speak("Sorry my friend  sattwik bhai. I am not able to send this email")   
+        elif 'exit' in query:
+            break
